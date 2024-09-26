e2f1d89538c3ec53b4b00330322ca3839c7d1424
Muneermadprit <muneerkulpdm1998@gmail.com>
2024-09-26 17:47:21 +0530

    api url change

--- src/components/Navbar/hovertest.jsx
+++ src/components/Navbar/hovertest.jsx
@@ -79,8 +79,8 @@ const AnimatedContainer = ({ onScrollToSubscription }) => {
   }, []);
 
   return (
-    <div className='hidden lg:flex md:bottom-[1200px] sm:hidden   '>
-      <div className="lg:flex   w-full h-screen justify-center items-center bg-gray-200 overflow-auto">
+    <div className='hidden lg:flex md:bottom-[900px] sm:hidden   '>
+      <div className="lg:flex   w-full h-screen bg-red-500 justify-center items-center bg-gray-200 overflow-auto">
         <div
           style={{
             transition: 'width 1s ease-in-out, height 1s ease-in-out, border-radius 1s ease-in-out, top 1s ease-in-out',
